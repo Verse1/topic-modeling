5dc4d36934db7914984c44b3a9952a5e7a446ba9
Verse <verse1@@users.noreply.github.com>
2023-10-12 22:26:27 -0400

    ali fix it

--- pdfConvert.py
+++ pdfConvert.py
@@ -1,14 +1,29 @@
 import os
 import re
 import pdfplumber
+import copy
 
 
 def extract_title_abstract(pdf_path):
     with pdfplumber.open(pdf_path) as pdf:
         page = pdf.pages[0]
         text = page.extract_text(x_tolerance=1, y_tolerance=5)
-        # print(text)
+        table={}
+        for x,i in enumerate(page.chars):
+            # print(i["text"])
+            if i["size"] not in table:
+                table[i["size"]]=""
+            if i["text"]==" ":
+                table[page.chars[x-1]["size"]]+=" "
+            table[i["size"]]+=i["text"]
+        print(text,"\n")
+        tmp=copy.deepcopy(table)
 
+        for i in tmp:
+            if len(tmp[i])<30:
+                del table[i]
+
+        print(table[max(table.keys())],"\n")
         abstract_match = re.search(r"Abstract\s+(.*?[.!?])\s*\n[c,©]", text, re.DOTALL)
         if not abstract_match:
             abstract_match = re.search(r'(?i)abstract\b\.?:?]?\s+((?:.|\n)+?)(?=\n\d+\.|Key|Introduction|[0,1]?\Z)', text, re.DOTALL)
@@ -21,7 +36,7 @@ def extract_title_abstract(pdf_path):
         abstract = "No abstract found"
     # abstract = text.split('\n')
     # print("Title: ",title,"Abstract: ",abstract)
-    print(abstract, "\n")
+    print(title, "\n")
 
     # return title, abstract
 
@@ -30,7 +45,7 @@ pdf_dir = "./data"
 pdf_files = [f for f in os.listdir(pdf_dir) if f.endswith(".pdf")]
 title_abstract_list = []
 
-testDir="./data/doc21.pdf"
+testDir="./data/doc1.pdf"
 print(testDir, os.path.join(testDir, testDir) )
 extract_title_abstract(testDir)
 
@@ -39,10 +54,10 @@ extract_title_abstract(testDir)
 #     title_abstract_list.append(title_abstract)
 # title_abstract
 
-for pdf_file in pdf_files:
-    pdf_path = os.path.join(pdf_dir, pdf_file)
-    print(pdf_file)
-    title_abstract = extract_title_abstract(pdf_path)
+# for pdf_file in pdf_files:
+#     pdf_path = os.path.join(pdf_dir, pdf_file)
+#     print(pdf_file)
+#     title_abstract = extract_title_abstract(pdf_path)
 
 
     # if title_abstract:
